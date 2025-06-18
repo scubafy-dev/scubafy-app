4351c20a16b9b7e9f0b61d0fe0b36d37edef159f
Alfred Pithu <alfredpithu@gmail.com>
2025-06-18 11:58:57 +0600

    push to deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import DashboardClient from "@/app/client";
 import { useAuth } from "@/lib/use-auth";
 
+
 export default async function DashboardPage() {
   const session = await useAuth("/");
   return (
